da88cd78a6ef540e9be42458a1459b4e3893ab05
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-12-05 00:02:37 +0200

    T1059.001

---
+++ MiTRE/T1059.001.V1.cpp
@@ -0,0 +1,21 @@
+// Link      = https://attack.mitre.org/techniques/T1059/001/
+// Technique = Command and Scripting Interpreter: PowerShell
+// ID        = T1059.001
+// Tactics   = Execution,
+
+#include <windows.h>
+
+
+int main() {
+    STARTUPINFOA si = { sizeof(STARTUPINFOA) };
+    PROCESS_INFORMATION pi = { 0 };
+
+    if (CreateProcessA(NULL,(LPSTR)"powershell.exe Start-Process -FilePath calc.exe",NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL,NULL,&si,&pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+
+    return 0;
+}
+
