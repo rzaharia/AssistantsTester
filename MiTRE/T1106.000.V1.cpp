3a2a4b66838d0e0dc2b47e729300be15af074e96
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-12-05 00:21:10 +0200

    T1106

---
+++ MiTRE/T1106.000.V1.cpp
@@ -0,0 +1,50 @@
+// Link      = https://attack.mitre.org/techniques/T1106/
+// Technique = Native API
+// ID        = T1106
+// Tactics   = Execution,
+
+#include <windows.h>
+
+#include <windows.h>
+#include <iostream>
+#include <winternl.h>
+
+typedef NTSTATUS(NTAPI* NtCreateProcess)(
+    PHANDLE ProcessHandle,
+    ACCESS_MASK DesiredAccess,
+    POBJECT_ATTRIBUTES ObjectAttributes,
+    HANDLE ParentProcess,
+    BOOLEAN InheritObjectTable,
+    HANDLE SectionHandle,
+    HANDLE DebugPort,
+    HANDLE ExceptionPort
+    );
+
+int main() {
+    HMODULE ntdll = LoadLibraryA("ntdll.dll");
+    NtCreateProcess pNtCreateProcess = (NtCreateProcess)GetProcAddress(ntdll, "NtCreateProcess");
+    if (!pNtCreateProcess) {
+        FreeLibrary(ntdll);
+        return 1;
+    }
+
+    HANDLE parentProcess = GetCurrentProcess();
+    HANDLE newProcessHandle = nullptr;
+    if (pNtCreateProcess(&newProcessHandle,PROCESS_ALL_ACCESS,NULL,parentProcess,FALSE,NULL,NULL,NULL) != 0) {
+        FreeLibrary(ntdll);
+        return 1;
+    }
+
+    STARTUPINFOA si = { sizeof(STARTUPINFOA) };
+    PROCESS_INFORMATION pi = { 0 };
+
+    if (CreateProcessA("calc.exe",NULL,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi))
+    {
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+    CloseHandle(newProcessHandle);
+    FreeLibrary(ntdll);
+
+    return 0;
+}
